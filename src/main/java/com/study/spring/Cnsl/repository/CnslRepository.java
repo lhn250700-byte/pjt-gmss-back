2eec4e24c7f3c13260b27519201ce12a685a896e
sephinote <sephinote@gmamil.com>
2026-03-15 00:26:04 +0900

    f1.1

--- src/main/java/com/study/spring/Cnsl/repository/CnslRepository.java
+++ src/main/java/com/study/spring/Cnsl/repository/CnslRepository.java
@@ -431,7 +431,7 @@ public interface CnslRepository extends JpaRepository<Cnsl_Reg, Long> {
             ci.cnsl_5_price  AS cnsl5Price,
             ci.cnsl_6_price  AS cnsl6Price
         from member m -- member전제
-        join member_role_list ml on m.member_id = ml.member_member_id and ml.role = 1  -- 상담사만 (1=SYSTEM)
+        join member_role_list ml on m.member_id = ml.member_member_id and ml.member_role_list = 1  -- 상담사만 (1=SYSTEM, DB 컬럼명은 member_role_list)
         left join (
         	select member_id,
                max(case when cnsl_tp = '1' then cnsl_price else 0 end ) cnsl_1_price,
@@ -512,7 +512,7 @@ public interface CnslRepository extends JpaRepository<Cnsl_Reg, Long> {
 		    ci.cnsl_5_price  AS cnsl5Price,
 		    ci.cnsl_6_price  AS cnsl6Price
 		from member m -- member전제
-		join member_role_list ml on m.member_id = ml.member_member_id and ml.role = 1  -- 상담사만 (1=SYSTEM)
+		join member_role_list ml on m.member_id = ml.member_member_id and ml.member_role_list = 1  -- 상담사만 (1=SYSTEM, DB 컬럼명은 member_role_list)
 		left join (
 		    select member_id,
 		           max(case when cnsl_tp = '1' then cnsl_price else 0 end ) cnsl_1_price,
