85c330036c0b05274be34503d6df8d984c6f59d7
sephinote <sephinote@gmamil.com>
2026-03-14 16:05:55 +0900

    JWT

--- src/main/java/com/study/spring/security/filter/JWTCheckFilter.java
+++ src/main/java/com/study/spring/security/filter/JWTCheckFilter.java
@@ -114,7 +114,6 @@ public class JWTCheckFilter extends OncePerRequestFilter {
         }
 
         try {
-            String accessToken = authHeaderStr.substring(7);
             Map<String, Object> claims = JWTUtil.validateToken(accessToken);
 
             log.info("JWT claims: {}", claims);
