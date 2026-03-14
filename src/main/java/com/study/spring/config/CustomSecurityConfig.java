d83aef1321c66072761b98489235dcb86158f4c5
sephinote <sephinote@gmamil.com>
2026-03-14 15:59:29 +0900

    dev ++

--- src/main/java/com/study/spring/config/CustomSecurityConfig.java
+++ src/main/java/com/study/spring/config/CustomSecurityConfig.java
@@ -1,3 +1,22 @@
+import com.study.spring.Member.service.CustomOAuth2UserService;
+import com.study.spring.security.filter.JWTCheckFilter;
+import com.study.spring.security.handler.APILoginFailHandler;
+import com.study.spring.security.handler.APILoginSuccessHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 @Configuration
 @Log4j2
 @EnableMethodSecurity
