d9ed72b73371f521a81fa337a4ddcb8159de96ca
sephinote <sephinote@gmamil.com>
2026-03-16 00:03:19 +0900

    f 1.2

--- src/main/java/com/study/spring/Cnsl/repository/CnslRepository.java
+++ src/main/java/com/study/spring/Cnsl/repository/CnslRepository.java
@@ -11,6 +11,7 @@ import com.study.spring.Member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
